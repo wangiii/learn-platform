ad35f11f4e12e5e58bec0e8727297487fd53d5dd
JasonVV <jasonwangiii@outlook.com>
2020-01-05 16:10:41 +0800

    init project

---
+++ src/main/java/com/angiii/learnplatform/LearnPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.angiii.learnplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angiii/learnplatform/LearnPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angiii.learnplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
