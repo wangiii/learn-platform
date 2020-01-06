8d662579c95e3d32887d5c2c04b03cb9686227e0
JasonVV <jasonwangiii@outlook.com>
2020-01-06 23:27:05 +0800

    add Faculty & Major

---
+++ src/main/java/com/angiii/learnplatform/model/Faculty.java
@@ -0,0 +1,24 @@
+package com.angiii.learnplatform.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Faculty extends BaseEntity implements Serializable {
+
+    /**
+     * 院系名称
+     */
+    private String name;
+
+    /**
+     * 专业列表
+     */
+    private List<Major> majors;
+}
---
+++ src/main/java/com/angiii/learnplatform/model/Major.java
@@ -0,0 +1,18 @@
+package com.angiii.learnplatform.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Major extends BaseEntity implements Serializable {
+
+    /**
+     * 专业名称
+     */
+    private String name;
+}
