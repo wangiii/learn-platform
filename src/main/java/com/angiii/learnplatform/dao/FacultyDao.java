2898a9282a8f11c49f62d408d04e082d1e1cc25d
JasonVV <jasonwangiii@outlook.com>
2020-01-09 16:31:38 +0800

    init dao

---
+++ src/main/java/com/angiii/learnplatform/dao/FacultyDao.java
@@ -0,0 +1,13 @@
+package com.angiii.learnplatform.dao;
+
+import com.angiii.learnplatform.model.Faculty;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FacultyDao {
+
+    @Insert("insert into tb_faculty(name, created_at, updated_at) " +
+            "values(#{name}, #{createTime}, #{updateTime})")
+    int insert(Faculty faculty);
+}
---
+++ src/main/java/com/angiii/learnplatform/dao/StudentDao.java
@@ -0,0 +1,7 @@
+package com.angiii.learnplatform.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StudentDao {
+}
