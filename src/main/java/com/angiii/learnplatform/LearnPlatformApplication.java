f9683dcfa33d476025d727fd0c0e7cf575a59d37
JasonVV <jasonwangiii@outlook.com>
2020-01-16 18:06:43 +0800

    fix timezone bug

--- src/main/java/com/angiii/learnplatform/LearnPlatformApplication.java
+++ src/main/java/com/angiii/learnplatform/LearnPlatformApplication.java
@@ -3,18 +3,10 @@ package com.angiii.learnplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class LearnPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnPlatformApplication.class, args);
-	}
-
-	@PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 	}
 }
