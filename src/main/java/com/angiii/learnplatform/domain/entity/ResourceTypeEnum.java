37f838e85b197f918781df7ac641243e0b8120eb
JasonVV <jasonwangiii@outlook.com>
2020-01-30 18:13:07 +0800

    add 教学资源枚举类

---
+++ src/main/java/com/angiii/learnplatform/domain/entity/ResourceTypeEnum.java
@@ -0,0 +1,12 @@
+package com.angiii.learnplatform.domain.entity;
+
+import java.io.Serializable;
+
+public enum ResourceTypeEnum implements Serializable {
+    VIDEO, // 教学视频
+    PPT, // 课件
+    HOMEWORK, // 作业
+    CASE, // 案例
+    INSTRUCTIONBOOK, //实验指导书
+    SYLLABUS // 课程大纲
+}
